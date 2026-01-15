c0b5256eff16e7ecf9c290cb7dea76e40c0e899e
onpe5679 <onpe@studioelysian.kr>
2026-01-15 15:22:45 +0900

    배포 오류 수정

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,11 +4,16 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
       },
     ],
   },
